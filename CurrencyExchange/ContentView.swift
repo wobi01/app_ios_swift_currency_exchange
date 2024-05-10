56cf254760a8b4cf5805dbb16abf072deb629885
Wojciech <wbiskup@edu.cdv.pl>
2024-05-10 19:13:03 +0200

    Initial Commit

---
+++ CurrencyExchange/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CurrencyExchange
+//
+//  Created by Wobi on 10/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CurrencyExchange/CurrencyExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyExchangeApp.swift
+//  CurrencyExchange
+//
+//  Created by Wobi on 10/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
